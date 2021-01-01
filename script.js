98a707acfc765fa609687de0b0bac2982d87244c
KowalewskiPawel <53332984+KowalewskiPawel@users.noreply.github.com>
2021-01-01 23:28:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("keydown", play);
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) =>
+  key.addEventListener("click", function () {
+    const audio = document.querySelector(`audio[data-key='${key.id}']`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+    setTimeout(function () {
+      key.classList.remove("playing");
+    }, 100);
+  })
+);
+
+function play(e) {
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+  setTimeout(function () {
+    key.classList.remove("playing");
+  }, 100);
+}
